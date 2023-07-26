945f4aaf088fd54ed1ca5e88417a3c64b2a5d3f9
Ayelsh <15768206596@com>
2023-07-26 11:19:38 +0800

    Ayelsh First Commit

---
+++ a235Admin/src/main/java/com/a235/a235admin/A235AdminApplication.java
@@ -0,0 +1,13 @@
+package com.a235.a235admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A235AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A235AdminApplication.class, args);
+    }
+
+}
---
+++ a235Admin/src/test/java/com/a235/a235admin/A235AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a235.a235admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A235AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
